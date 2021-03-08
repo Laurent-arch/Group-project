275f85b23df19f4eb784eacbf072ee7c48031c2f
Laurent-arch <72408402+Laurent-arch@users.noreply.github.com>
2021-03-08 13:35:44 +0200

    Add files via upload

---
+++ my_group_files/myscript.js
@@ -0,0 +1,38 @@
+function printerText(elemId, hintMsg) {
+  document.getElementById(elemId).innerHTML = hintMsg;
+}
+
+function validateForm(){
+    // const email = document.loginForm.email.value;
+    const pswd = document.loginForm.pswd.value;
+
+    // var emailError = true;
+    var pswdError = true;
+
+    // if(email == ""){
+    //     printerText('emailError', 'Please enter a valid email')
+    // }
+    // else {
+    //     var regex = /^\S+@\S+\.\S+$/;
+    //     if (regex.test(email) === false){
+    //         printerText('emailError', 'Please enter a valid email')
+    //     }
+    //     else {
+    //         printerText('email Error', '')
+    //     } emailError = false;
+    // }
+
+    if (pswd == ""){
+        printerText('pswdError', "Please enter your password")
+    }
+    else {
+        var regex = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/;
+        if(regex.test(pswd) === false){
+            printerText('pswd', "Min: 6 character in form Asd4$1")
+        }
+        else {
+            printerText('pswdError', "")
+            pswdError = false;
+        }
+    }
+}
